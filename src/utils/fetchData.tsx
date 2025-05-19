0e0bacf52fdde665874fcccace70314f5f97ef23
KristofPhillips95 <kristofphillips@gmail.com>
2025-05-19 17:51:20 +0200

    Change data fetching urls

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -2,7 +2,9 @@ export async function fetchAsJson(lts: boolean) {
   if (lts) {
     try {
         const response = await fetch(
-          "https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/lts_items_dev"
+          // "https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/lts_items_dev"
+          "https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/lts_items_v2"
+
         );
         if (!response.ok) throw new Error("Network response was not ok");
         return await response.json();
@@ -14,7 +16,9 @@ export async function fetchAsJson(lts: boolean) {
   else  {
     try {
       const response = await fetch(
-        "https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/items_dev"
+        // "https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/items_dev"
+        "https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/items"
+
       );
       if (!response.ok) throw new Error("Network response was not ok");
       return await response.json();
